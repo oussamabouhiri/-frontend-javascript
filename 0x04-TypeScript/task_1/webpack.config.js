504ef38885b49da06a745f02b6cfda12ad5f9f02
OUSSAMA BOUHIRI <oussamabouhiri@gmail.com>
2025-09-28 23:57:05 +0100

    Add webpack configuration for TypeScript project

--- 0x04-TypeScript/task_1/webpack.config.js
+++ 0x04-TypeScript/task_1/webpack.config.js
@@ -1,1 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/main.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 8080,
+    open: true,
+  },
+  mode: "development",
+};
 
