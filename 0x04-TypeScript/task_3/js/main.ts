fc0991cecdf8a1794aeede63b9654b80b95223e3
OUSSAMA BOUHIRI <oussamabouhiri@gmail.com>
2025-09-29 11:15:58 +0100

    Implement CRUD operations for Student object

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,1 +1,27 @@
+// task_3/js/main.ts
+
+import { Student } from "./interface";
+import { insertRow, deleteRow, updateRow } from "./crud";
+
+// Create a student
+const student: Student = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+  age: 23,
+  location: "San Francisco",
+};
+
+// Insert student
+const newRowID = insertRow(student);
+
+// Update student
+updateRow(newRowID, {
+  firstName: "Oussama",
+  lastName: "Bouhiri",
+  age: 26,
+  location: "Agadir",
+});
+
+// Delete student
+deleteRow(newRowID);
 
