e1cb9cb130de483c68410b01a1aa97ee3e38c1e4
OUSSAMA BOUHIRI <oussamabouhiri@gmail.com>
2025-09-29 11:14:28 +0100

    Add CRUD function declarations for Student

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,1 +1,8 @@
+// task_3/js/crud.d.ts
+
+import { Student } from "./interface";
+
+export function insertRow(student: Student): number;
+export function deleteRow(rowId: number): void;
+export function updateRow(rowId: number, student: Student): number;
 
