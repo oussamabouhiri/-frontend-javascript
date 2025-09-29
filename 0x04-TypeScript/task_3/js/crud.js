fb27f26add5720ea0d33d3b7414bd36893256999
OUSSAMA BOUHIRI <oussamabouhiri@gmail.com>
2025-09-29 11:15:32 +0100

    Add CRUD functions for student records

--- 0x04-TypeScript/task_3/js/crud.js
+++ 0x04-TypeScript/task_3/js/crud.js
@@ -1,1 +1,16 @@
+// task_3/js/crud.js
+
+export function insertRow(student) {
+  console.log("Insert row", student);
+  return Math.floor(Math.random() * 1000); // return a fake ID
+}
+
+export function deleteRow(rowId) {
+  console.log("Delete row id", rowId);
+}
+
+export function updateRow(rowId, student) {
+  console.log(`Update row ${rowId}`, student);
+  return rowId;
+}
 
